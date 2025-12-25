d2f3e237549a431c60c523126e2bc1c43dc4f6fa
Raden Salman Alfaridzi <80512930+rsalmn@users.noreply.github.com>
2025-12-26 06:30:47 +0700

    Improve UI loading with error handling
    Added error handling for UI loading and updated window title.

--- NPN_Hub.lua
+++ NPN_Hub.lua
@@ -1,12 +1,29 @@
 repeat task.wait() until game:IsLoaded()
 task.wait(1)
 
-local WindUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/Footagesus/WindUI/main/main.lua"))()
+print("[NPN] Loading UI...")
+
+local succ, WindUI = pcall(function()
+    return loadstring(game:HttpGet("https://raw.githubusercontent.com/Footagesus/WindUI/main/main.lua"))()
+end)
+
+if not succ or not WindUI then
+    warn("[NPN] WindUI gagal load")
+    return
+end
 
 local Window = WindUI:CreateWindow({
-    Title = "ROCKHUB TEST UI",
-    Icon = "rbxassetid://116236936447443"
+    Title = "NPN HUB TEST UI",
+    Icon = "rbxassetid://116236936447443",
 })
 
-task.wait(1)
-Window:Toggle(true)
+print("[NPN] UI Created")
+
+task.delay(1, function()
+    if Window and Window.Toggle then
+        Window:Toggle(true)
+        print("[NPN] UI Opened")
+    else
+        warn("[NPN] Window exists tapi ga punya Toggle() ??")
+    end
+end)
