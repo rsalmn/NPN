0f1a485be0afc4bf41bc81bd594819d52a5fcbd5
Raden Salman Alfaridzi <80512930+rsalmn@users.noreply.github.com>
2025-12-26 06:37:46 +0700

    Update NPN_Hub.lua

--- NPN_Hub.lua
+++ NPN_Hub.lua
@@ -1,11 +1,20 @@
-repeat task.wait() until game:IsLoaded()
-task.wait(1)
-
-print("[NPN] Script Running...")
-
-local ok, WindUI = pcall(function()
-    return loadstring(game:HttpGet("https://github.com/Footagesus/WindUI/releases/latest/download/main.lua"))()
-end)
+local WindUI = loadstring(game:HttpGet("https://github.com/Footagesus/WindUI/releases/latest/download/main.lua"))()
+local Window = WindUI:CreateWindow({
+    Title = "RockHub - Fish It",
+    Icon = "rbxassetid://116236936447443",
+    Author = "Premium Version",
+    Folder = "RockHub",
+    Size = UDim2.fromOffset(600, 360),
+    MinSize = Vector2.new(560, 250),
+    MaxSize = Vector2.new(950, 760),
+    Transparent = true,
+    Theme = "Rose",
+    Resizable = true,
+    SideBarWidth = 190,
+    BackgroundImageTransparency = 0.42,
+    HideSearchBar = true,
+    ScrollBarEnabled = true,
+})
 
 print("[NPN] WindUI:", ok, WindUI)
 
@@ -22,3 +31,4 @@ local Window = WindUI:CreateWindow({
 task.wait(1)
 Window:Toggle(true)
 print("DONE")
+
